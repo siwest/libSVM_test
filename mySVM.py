8e4e2c32524e0a7a976457d114b34d8462fdfc76
Sarah West <sarah.irene.west@gmail.com>
2016-08-02 16:55:10 -0700

    Corrects input processing

--- mySVM.py
+++ mySVM.py
@@ -2,7 +2,7 @@ from svmutil import *
 import sys
 
 
-def readFiletoList(textFile):
+def readDatatoList(textFile):
 	f = open(textFile)
 	data = []
 	i = 0
@@ -12,18 +12,29 @@ def readFiletoList(textFile):
 	while (l != ''):
 		a = l.split()
 		l2 = []
-		for j in range (0, len(a), 1):
-			l2.append(int(a[j]))
+		for j in range (0, len(a)/100, 1):
+			l2.append(float(a[j]))
 		data.append(l2)
 		l = f.readline()
 	f.close()
 	return data
 
+def readLabelstoList(labelFile):
+	f = open(labelFile)
+	data = []
+	i = 0
+	l = f.readline()
+	# Read Data
+	while (l != ''):
+		a = l.split()
+		data.append(int(a[0]))
+		l = f.readline()
+	f.close()
+	return data
+
 def main(data, labels):
-    dataList = readFiletoList(data)
-    print "dataList", dataList[0]
-    labelList = readFiletoList(labels)
-    print "labelList", labelList[0]
+    dataList = readDatatoList(data)
+    labelList = readLabelstoList(labels)
     model = svm_train(labelList, dataList)
 
 
